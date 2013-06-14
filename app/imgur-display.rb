424a28bad29637798562ec3f6a057303211902a1
Joshua Lund <josh@joshlund.com>
2013-06-14 15:47:09 -0600

    Arbitary commit to demonstrate deploying new code

--- app/imgur-display.rb
+++ app/imgur-display.rb
@@ -7,7 +7,7 @@ module Imgur
   class Display < Sinatra::Application
 
     get "/" do
-      "<h1>imgur!</h1><br /><img src=\"#{ Imgur::Random.get }\" />"
+      "<img src=\"#{ Imgur::Random.get }\" />"
     end
 
   end
